1347f36a7db0b0128c4f2eed72a4c0e4f944da4d
Tobias Widlund <widlundtobias@gmail.com>
2018-10-11 21:16:22 +0200

    get rid of comments

--- server/main.go
+++ server/main.go
@@ -47,65 +47,10 @@ func rootCmdFunc(cmd *cobra.Command, args []string) error {
 		log.Printf("Config loaded")
 	}
 
-	//services
-	//var dogService dog.Service
-
-	//initialise services
-	//switch cfg.StorageMode {
-	//case "mysql":
-	//	//database migration
-	//	if err := util.MysqlMigrateUp(cfg.DatabaseConnection, cfg.MigrateDir); err != nil {
-	//		return err
-	//	}
-
-	//	//create database instance that services will use
-	//	db, err := util.MysqlConnect(cfg.DatabaseConnection)
-	//	if err != nil {
-	//		return err
-	//	}
-
-	//	dogService = dog.NewSQLService(db)
-	//case "dummy":
-	//	//dogService = dog.NewDummySerbice(db)
-	//default:
-	//	return fmt.Errorf("Invalid storageMode: %s", cfg.StorageMode)
-	//}
-	//defer func() {
-	//	ctx, cancel := context.WithTimeout(context.Background(), 2*time.Second)
-	//	dogService.Close(ctx)
-	//	cancel()
-	//}()
-
-	//managers
-	//dogManager := dog.NewManager(dogService)
-
-	//api
-	//dogAPI := dogapi.New(dogManager)
-
-	//routes
-	//dogAPI.RegisterRoutes(router)
-
 	//if you also serve static files from this application:
-	// strip the prefix "static" and serve files from the directory "static"
 	// router.Get takes a handler func, so append .ServeHTTP to the end of the handler
 	//router.Get("/", http.StripPrefix("/site/dist/", http.FileServer(http.Dir("site/dist/c"))).ServeHTTP)
 	router.Get("/**", http.FileServer(http.Dir("site/dist")).ServeHTTP)
-
-	//fs := http.FileServer(http.Dir(dir))
-	//http.Handle("/", fs)
-	//http.HandleFunc("/filter/", func(w http.ResponseWriter, r *http.Request) {
-	//	http.ServeFile(w, r, "index.html")
-	//})
-	//router.Handle("/**", staticFs)
-
-	// Below Applies Local CORS capabilities per Resource (both methods covered)
-	// by default this will merge the "GlobalCors" settings with the resource
-	// cors settings.  Without specifying the AllowMethods, the router will
-	// accept any Request-Methods that have valid handlers associated
-	//router.SetCors("/welcome", &vestigo.CorsAccessControl{
-	//	AllowMethods: []string{"GET"},                    // only allow cors for this resource on GET calls
-	//	AllowHeaders: []string{"X-Header", "X-Z-Header"}, // Allow this one header for this resource
-	//})
 
 	//log.Fatal(http.ListenAndServeTLS(":" + cfg.Port, "MyCertificate.crt", "MyKey.key", router))
 	log.Fatal(http.ListenAndServe(":"+strconv.Itoa(cfg.Port), router))
