aa0df0ce56046641d07d626953e1e9aa86988c13
Tobias Widlund <widlundtobias@gmail.com>
2018-10-13 21:13:07 +0200

    add traits item

--- site/src/components/traitsinspector.vue
+++ site/src/components/traitsinspector.vue
@@ -1,11 +1,12 @@
 <template>
     <div>
         <h2>Traits</h2>
-        <p>std::is_aggregate: {{ is_aggregate }}</p>
+        <traits-item v-for="trait in traits" v-bind:item_data="trait"></traits-item>
     </div>
 </template>
 
 <script>
+import TraitsItem from 'components/traitsitem.vue'
 import { is_aggregate } from 'type_traits/type_traits.ts'
 
 export default {
@@ -16,6 +17,25 @@ export default {
         'is_aggregate': function () {
             return is_aggregate(this.type_description);
         },
+        'traits': function () {
+            return [
+                this.create_traits_item("std::is_aggregate", is_aggregate),
+            ];
+        },
+    },
+    methods: {
+        create_traits_item (name, traits_function) {
+            var traits_result = traits_function(this.type_description);
+
+            return {
+                name: name,
+                is_true: traits_result.is_true,
+                reasons: traits_result.reasons,
+            };
+        },
+    },
+    components: {
+        'traits-item': TraitsItem,
     },
 }
 </script>
---
+++ site/src/components/traitsitem.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <p>{{ item_data.name }} - {{ item_data.is_true }} | {{ reason_string }} </p>
+    </div>
+</template>
+
+<script>
+import { Attribute } from 'type_traits/type_traits.ts'
+export default {
+    props: [
+        'item_data',
+    ],
+    computed: {
+        'reason_string': function () {
+            let result = "";
+
+            this.item_data.reasons.forEach(function(val, i) {
+                if(val)
+                {
+                    result += Attribute[i] + " ";
+                }
+            });
+
+            return result;
+        },
+    }
+}
+</script>
